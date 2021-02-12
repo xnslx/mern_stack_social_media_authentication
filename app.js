57294cb77e5a44b7fe2e1c200aa37ad04235881d
xnslx <xnslx89@gmail.com>
2021-02-11 23:50:10 -0600

    connect to db

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const dbUrl = process.env.URL;
+const port = process.env.PORT;
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Mongodb connect successfully')
+}).catch(err => {
+    console.log(err)
+})
+
+app.listen(port)
