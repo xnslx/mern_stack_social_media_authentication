99b2ff5316fcb81c744b67832302cc37b5caed66
xnslx <xnslx89@gmail.com>
2021-02-19 21:05:34 -0600

    set up the client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>MERN STACK SOCIAL MEDIA AUTHENTICATION</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
